d91fd1d72b86ae22af53eecd19fb1ec1048e85f9
STEPHEN MIDDLEDITCH <W9283667@live.tees.ac.uk>
2022-03-11 13:28:55 +0000

    1stLargeCommit

---
+++ app/src/main/java/com/example/textrecognitionapp/MainActivity.java
@@ -0,0 +1,63 @@
+package com.example.textrecognitionapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity
+{
+    private Button captureImageBtn, detectTextBtn;
+    private ImageView imageView;
+    private TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        captureImageBtn = findViewById(R.id.capture_image_btn);
+        detectTextBtn = findViewById(R.id.detect_text_image_btn);
+        imageView = findViewById(R.id.image_view);
+        textView = findViewById(R.id.text_display);
+
+        captureImageBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+    }
+    //// Bellow code taken from https://developer.android.com/training/camera/photobasics#java
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        try {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        } catch (ActivityNotFoundException e) {
+            // display error state to the user
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK)
+        {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            imageView.setImageBitmap(imageBitmap);
+        }
+    }
+    //// Above code taken from https://developer.android.com/training/camera/photobasics#java
+}
